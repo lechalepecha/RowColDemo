3d7ee45868b0333f9e94b41971fda954c86e6f25
lechalepecha <aleksejserakov45@gmail.com>
2025-09-28 15:32:33 +0700

    filament in end

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -46,18 +46,10 @@ class MainActivity : ComponentActivity() {
 fun MainScreen(modifier: Modifier = Modifier){
 
     Row{
-        Text(
-            text = "Large text\n \nMore text",
-            Modifier.alignBy(FirstBaseline),
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold)
-        Text(
-            text = "Small text",
-            modifier = Modifier.paddingFrom(
-                alignmentLine = FirstBaseline, before = 80.dp, after = 0.dp
-            ),
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold)
+        TextCell("1", Modifier.weight(weight = 0.2f, fill = true))
+        TextCell("2", Modifier.weight(weight = 0.4f, fill = true))
+        TextCell("3", Modifier.weight(weight = 0.3f, fill = true))
+
     }
 
 }
