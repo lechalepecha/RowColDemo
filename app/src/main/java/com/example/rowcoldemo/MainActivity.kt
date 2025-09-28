44f1c66306c19a298bf697d9aed768676cc5bfd4
lechalepecha <aleksejserakov45@gmail.com>
2025-09-28 14:52:50 +0700

    Column

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier){
 
-    Row(modifier){
+    Column(modifier){
         TextCell("1")
         TextCell("2")
         TextCell("3")
